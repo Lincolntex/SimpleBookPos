9da45e26a5115252a7a8adbea277486d9d969229
Jacob <jakeleveroni@yahoo.com>
2017-08-20 23:32:20 -0700

    added references to all data models in the Testing suite class

--- js/tests/TestingSuite.js
+++ js/tests/TestingSuite.js
@@ -1,9 +1,14 @@
+var UserModel = require('../models/UserModel.js').UserModel;
+var BookModel = require('../models/BookModel.js').BookModel;
+var AppointmenModel = require('../models/AppointmentModel.js').AppointmentModel;
+var TransactionModel = require('../models/TransactionModel.js').TransactionModel;
+
 var DatabaseManager = require('../DataBaseManager.js').DataBaseManager;
 var db = new DatabaseManager();
 
 class TestingSuite {
     constructor() {
-
+        
     }
 
     // define very carefully architected test suites here
